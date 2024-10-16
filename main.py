2b005959767e3761fb8cfb96940015c003099117
Soundarya <soundarya.g@innoboon.com>
2024-10-16 11:49:35 +0530

    structured apis into separate folder

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ models.Base.metadata.create_all(engine)
 
 from middleware import middleware
 from authentication import auth
+from services import service_routers
 app.include_router(auth.router)
+app.include_router(service_routers.router)
